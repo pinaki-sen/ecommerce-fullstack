c2a60c904ed542d451169a64c178c7ab059beab3
PINAKI SEN <pinaki.code@gmail.com>
2020-05-13 10:17:44 +0530

    backend commited

---
+++ project_backend/app.js
@@ -0,0 +1,54 @@
+require('dotenv').config();
+
+const mongoose = require("mongoose");
+const express = require("express");
+const app = express();
+const bodyParser = require("body-parser");
+const cookieParser = require("cookie-parser");
+const cors = require("cors");
+
+//Importing my routes
+const authRoutes = require("./routes/auth.js");
+const userRoutes = require("./routes/user.js");
+const categoryRoutes = require("./routes/category.js");
+const productRoutes = require("./routes/product.js");
+const orderRoutes = require("./routes/order.js");
+const paymentBRoutes = require("./routes/paymentBRoutes");
+
+
+// DB connection
+mongoose.connect(process.env.DATABASE, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true
+}).then(() => {
+    console.log("DB CONNECTED");
+});
+
+
+// Middlewire
+app.use(bodyParser.json());
+app.use(cookieParser());
+app.use(cors());
+
+
+
+//My Routes
+app.use("/api", authRoutes);
+app.use("/api", userRoutes);
+app.use("/api", categoryRoutes);
+app.use("/api", productRoutes);
+app.use("/api", orderRoutes);
+app.use("/api", paymentBRoutes);
+
+
+
+//Port
+const port = process.env.PORT || 8000;
+
+
+
+//Starting a Server
+app.listen(port, () => {
+    console.log(`app is running at ${port}`);
+});
---
+++ project_backend/routes/category.js
@@ -0,0 +1,31 @@
+const express = require("express")
+const router = express.Router()
+
+const {getCategoryById, createCategory, getCategory, getAllCategory, updateCategory, removeCategory} = require("../controllers/category.js");
+const {isSignedIn, isAuthenticated, isAdmin} = require("../controllers/auth.js");
+const {getUserById} = require("../controllers/user.js");
+
+
+//params
+router.param("userId", getUserById);
+router.param("categoryId", getCategoryById);
+
+
+//actual routes goes here
+
+//create routes
+router.post("/category/create/:userId", isSignedIn, isAuthenticated, isAdmin, createCategory);
+
+//read routes
+router.get("/category/:categoryId", getCategory);
+router.get("/categories", getAllCategory);
+
+
+//update
+router.put("/category/:categoryId/:userId", isSignedIn, isAuthenticated, isAdmin, updateCategory);
+
+//delete
+router.delete("/category/:categoryId/:userId", isSignedIn, isAuthenticated, isAdmin, removeCategory);
+
+
+module.exports = router;
